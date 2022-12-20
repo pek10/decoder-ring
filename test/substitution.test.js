a73ce0d1e0df537ce8d90ef93e21b16d8119b13a
Alex Myers <alex@pek10.dev>
2022-12-19 21:17:16 -0500

    Added tests for substitution

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,40 @@
+const { expect } = require('chai')
+const { substitution } = require('../src/substitution')
+
 // Write your tests here!
+describe('substitution()', () => {
+  it('is a function', () => {
+    expect(substitution).to.be.a('function')
+  })
+
+  it('should return a string', () => {
+    const message = 'message'
+    const alphabet = 'qnpyhbluemiosdjwctfgazxrkv'
+    const actual = typeof substitution(message, alphabet)
+    const expected = 'string'
+
+    expect(actual).to.equal(expected)
+  })
+
+  describe('encoding a message', () => {
+    it('should encode "Alex Myers" properly', () => {
+      const message = 'Alex Myers'
+      const alphabet = 'qnpyhbluemiosdjwctfgazxrkv'
+      const actual = substitution(message, alphabet)
+      const expected = 'qohr skhtf'
+
+      expect(actual).to.equal(expected)
+    })
+  })
+
+  describe('decoding a message', () => {
+    it('should decode "Alex Myers" properly', () => {
+      const message = 'qohr skhtf'
+      const alphabet = 'qnpyhbluemiosdjwctfgazxrkv'
+      const actual = substitution(message, alphabet, false)
+      const expected = 'alex myers'
+
+      expect(actual).to.equal(expected)
+    })
+  })
+})
